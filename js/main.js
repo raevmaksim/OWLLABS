afc14b39c4e0c0b7890a247252afabeb6682a4dc
raevmaksim <raev_maksim_frontend@mail.ru>
2024-10-11 22:33:36 +0500

    form & pop up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+// burger=>mobile-menu
+
 burger.onclick = function (e) { 
   e.preventDefault()
   document.getElementById('navmenu').classList.remove('header__navmenu')
@@ -5,11 +7,32 @@ burger.onclick = function (e) {
     
   document.getElementById('burger').style.display='none'
   document.getElementById('close').style.display='block'
-  console.log(screenWidth) 
 }
-  window.addEventListener('resize', () => {if(window.screen.width > '878') {
+  window.addEventListener('resize', () => {
+    if(window.screen.width > '878') {
   document.getElementById('navmenu').classList.remove('header__navmenu_mobile')
   document.getElementById('navmenu').classList.add('header__navmenu') 
   document.getElementById('close').style.display='none'
 }
+})
+
+
+// btn=>form
+document.getElementById('btnNavmenu').addEventListener('click', function(e) { 
+  e.preventDefault()
+  document.getElementById('form').classList.remove('form')
+  document.getElementById('form').classList.add('form__open')
+})
+document.getElementById('btnSubtitle').addEventListener('click', function(e) { 
+  e.preventDefault()
+  document.getElementById('form').classList.remove('form')
+  document.getElementById('form').classList.add('form__open')
+})
+
+//btn-form=>popup
+document.getElementById('btnForm').addEventListener('click', function(e) { 
+  e.preventDefault()
+  document.getElementById('popup').classList.remove('popup')
+  document.getElementById('popup').classList.add('popup__open')
+  document.querySelector('.form__window').style.display='none'
 })
