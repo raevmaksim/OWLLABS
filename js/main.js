c02e6f801c5b50b5ad60c9bb56bb1832a3aaea87
raevmaksim <raev_maksim_frontend@mail.ru>
2024-10-11 12:50:47 +0500

    burger & close

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-burger.onclick = function () {
-  // navmenu.hidden = !navmenu.hidden  
-  document.getElementById('navmenu').style.display='block'
+burger.onclick = function (e) { 
+  e.preventDefault()
+  document.getElementById('navmenu').classList.remove('header__navmenu')
+  document.getElementById('navmenu').classList.add('header__navmenu_mobile')
+    
+  document.getElementById('burger').style.display='none'
+  document.getElementById('close').style.display='block'
+  console.log(screenWidth) 
 }
+  window.addEventListener('resize', () => {if(window.screen.width > '878') {
+  document.getElementById('navmenu').classList.remove('header__navmenu_mobile')
+  document.getElementById('navmenu').classList.add('header__navmenu') 
+  document.getElementById('close').style.display='none'
+}
+})
